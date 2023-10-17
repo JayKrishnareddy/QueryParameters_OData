70f54334007173b7fa5f2fca79f29667b7b82070
Jay Krishna <jay.krishna@consultant.volvo.com>
2023-10-17 09:19:13 +0200

    Removed the unused code from the repo

--- Odata/Controllers/WeatherForecastController.cs
+++ Odata/Controllers/WeatherForecastController.cs
@@ -8,29 +8,6 @@ namespace Odata.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
         [HttpGet(nameof(GetData))]
         [EnableQuery(AllowedQueryOptions = AllowedQueryOptions.All)]
         public List<Stores> GetData() => GetDummyData();
